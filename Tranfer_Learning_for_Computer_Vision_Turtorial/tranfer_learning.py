503ad9c3648897b85cd5d7742b64324229d34e2c
nileshBnova25 <nilesh1paerp2@gmail.com>
2026-08-24 15:21:11 +0530

    Lab_Submission

--- Tranfer_Learning_for_Computer_Vision_Turtorial/tranfer_learning.py
+++ Tranfer_Learning_for_Computer_Vision_Turtorial/tranfer_learning.py
@@ -170,6 +170,7 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
                 print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
 
+                "Check pointing "
                 # deep copy the model
                 if phase == 'val' and epoch_acc > best_acc:
                     best_acc = epoch_acc
@@ -246,31 +247,36 @@ def visualize_model(model, num_images=20):
         plt.show()
         model.train(mode=was_training)
 
-#----------------------Visualizing the model predictions-------------------------------------------------_____#
-"""This part is the main transfer learning is happening 
- the resnet18 already learned general visual feature such as 
- edges -> textures -> shapes -> patterns instead training from
- scratch we will start from already trained weight of resNet"""
-model_ft = models.resnet18(weights='IMAGENET1K_V1')
-num_ftrs = model_ft.fc.in_features
-# Here the size of each output sample is set to 2.
-# Alternatively, it can be generalized to ``nn.Linear(num_ftrs, len(class_names))``.
+#----------------------Visualizing the model predictions-------------------------------------------------#
+def main():
+    """This part is the main transfer learning is happening
+    the resnet18 already learned general visual feature such as
+    edges -> textures -> shapes -> patterns instead training from
+    scratch we will start from already trained weight of resNet"""
+    model_ft = models.resnet18(weights='IMAGENET1K_V1')
+    num_ftrs = model_ft.fc.in_features
+    # Here the size of each output sample is set to 2.
+    # Alternatively, it can be generalized to ``nn.Linear(num_ftrs, len(class_names))``.
 
-""" Imagenet has 1000 classes but the current classification problem has only 2 classes"""
-model_ft.fc = nn.Linear(num_ftrs, 2)
+    """ Imagenet has 1000 classes but the current classification problem has only 2 classes"""
+    model_ft.fc = nn.Linear(num_ftrs, 2)
 
-model_ft = model_ft.to(device)
+    model_ft = model_ft.to(device)
 
-criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss()
 
-# Observe that all parameters are being optimized
-"""lr -> learning rate, momentum -> helps SGD maintain some direction from previous updates"""
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    # Observe that all parameters are being optimized
+    """lr -> learning rate, momentum -> helps SGD maintain some direction from previous updates"""
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-#------------------------------------------------------------------------------------#
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    #------------------------------------------------------------------------------------#
 
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                        num_epochs=25)
-visualize_model(model_ft)
+    visualize_model(model_ft)
+
+    #-------------------------------------------------------------------------------------#
+if __name__ == '__main__':
+    main()
